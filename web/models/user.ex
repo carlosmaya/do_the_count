c5e387773f1c0e21376e4beaee7613e24b6bf2b2
carlos maya <beto@carlosmaya.mx>
2015-12-28 17:11:10 -0600

    Renames username to name and adds changeset validations

--- web/models/user.ex
+++ web/models/user.ex
@@ -2,13 +2,23 @@ defmodule DoTheCount.User do
   use DoTheCount.Web, :model
 
   schema "users" do
-    field :username, :string
+    field :name, :string
     field :email, :string
     field :balance, :float
     field :password, :string, virtual: true
     field :password_hash, :string
 
     timestamps
+  end
+
+  @require_fields ~w(name email)
+  @optional_fields ~w(balance)
+
+  def changeset(user, params \\ :empty) do
+    user
+    |> cast(params, @require_fields, @optional_fields)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint(:email)
   end
 
 end
