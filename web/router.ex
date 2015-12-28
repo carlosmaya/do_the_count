2afd0101f087480c02bb67e126fc4bf3693a1b63
carlos maya <beto@carlosmaya.mx>
2015-12-28 17:12:52 -0600

    Simplifies router

--- web/router.ex
+++ web/router.ex
@@ -16,9 +16,8 @@ defmodule DoTheCount.Router do
   scope "/", DoTheCount do
     pipe_through :browser # Use the default browser stack
 
-    get "/users", UserController, :index
-    get "/user/:id", UserController, :show
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
