435191bc7f54105cbb0aa6431ea63ca1a27375f6
carlos maya <beto@carlosmaya.mx>
2015-12-29 18:13:24 -0600

    adds functions to fix messages for limit validations

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,3 +1,10 @@
 defmodule DoTheCount.UserView do
   use DoTheCount.Web, :view
+
+  def translate_error({msg, opts}) do
+    String.replace(msg, "%{count}", to_string(opts[:count]))
+  end
+
+  def translate_error(msg), do: msg
+
 end
